852f392a60041a1d4e17a496ea3e1d23c62c40a7
Edward-ia <gaya@edward-ia.ca>
2026-07-07 08:23:32 -0400

    Add files via upload

--- pff_configurateur/__manifest__.py
+++ pff_configurateur/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Configurateur',
-    'version': '19.0.1.0.3',
+    'version': '19.0.1.0.4',
     'summary': "Configurateur de portes et fenêtres PVC, commandes et bons de travail",
     'author': 'Edward IA / KerningCode',
     'category': 'Manufacturing',
